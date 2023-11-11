be93019a935ac18e5fbda4789389784fd3e9f517
carlos mario toro martinez <carlostorom.96@gmail.com>
2023-11-11 02:30:34 -0400

    test CI/CD CodeDeploy

---
+++ scripts/after_install.sh
@@ -0,0 +1,5 @@
+cd /home/ubuntu/Backend_Strapi_Education
+sudo npm install -g pm2
+sudo npm install
+sudo npm run build
+
---
+++ scripts/application_start.sh
@@ -0,0 +1,2 @@
+sudo pm2 delete all
+sudo pm2 start npm --name "Backend_Strapi_Education" -- start
